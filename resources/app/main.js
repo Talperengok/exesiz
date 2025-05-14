2af20141ed0f1525e54f16fc91da2a7cf751c2e4
Talperengok <tevfikalperengok@gmail.com>
2025-05-14 19:05:43 +0300

    das

---
+++ resources/app/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  // Yeni bir pencere oluşturuyoruz
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // Güvenlik açısından Node entegrasyonunu kapatabilir, 
+      // eğer sadece web teknolojileri kullanacaksanız:
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js') // Eğer özel bir preload dosyası kullanacaksanız
+    }
+  });
+
+  // index.html dosyasını yüklüyoruz
+  win.loadFile('index.html');
+
+  // Opsiyonel: Geliştirici araçlarını açar
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    // macOS'ta aktif pencereler bitince yeniden yeni pencere açma
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Tüm pencereler kapatıldığında uygulamayı sonlandırır
+app.on('window-all-closed', function () {
+  // macOS'ta pencere kapatıldığında uygulama tamamen kapanmayabilir
+  if (process.platform !== 'darwin') app.quit();
+});
